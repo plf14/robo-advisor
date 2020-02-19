b2aa6f7ee6c8dc8eb9d6f09c13d49b6562908c20
plf14 <59637504+plf14@users.noreply.github.com>
2020-02-19 18:16:51 -0500

    Reading all data

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -4,13 +4,31 @@ import requests
 import json
 import os
 from dotenv import load_dotenv
+import datetime
 
 load_dotenv()
 
+def to_usd(my_price):
+    """
+    Converts a numeric value to usd-formatted string, for printing and display purposes.
+    Source: https://github.com/prof-rossetti/intro-to-python/blob/master/notes/python/datatypes/numbers.md#formatting-as-currency
+    Param: my_price (int or float) like 4000.444444
+    Example: to_usd(4000.444444)
+    Returns: $4,000.44
+    """
+    return f"${my_price:,.2f}" #> $12,000.71
+
+date = datetime.date.today()
+time = datetime.datetime.now()
+
+print("-------------------------")
+print("WELCOME TO THE ROBO STOCK ADVISOR")
+print("ENTER THE SYMBOL OF YOUR STOCK TO RECIEVE MY RECOMENDATION")
+Symbol = input("SYMBOL: ")
+print("-------------------------")
 print("REQUESTING SOME DATA FROM THE INTERNET...")
 print("-------------------------")
 
-Symbol = input("Symbol:  ")
 APIkey = os.getenv("ALPHAVANTAGE_API_KEY", default = "OOPS")
 
 request_url = ("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=" + Symbol + "&apikey=" + APIkey)
@@ -18,29 +36,42 @@ print("URL:", request_url)
 
 response = requests.get(request_url)
 parsed_response = json.loads(response.text)
+tsd = parsed_response["Time Series (Daily)"]
+Dates = list(tsd.keys())
+Opens = []
+Highs = []
+Lows = []
+Closes = []
+Volumes = []
+for Date in Dates:
+    Opens.append(tsd[Date]["1. open"])
+    Highs.append(tsd[Date]["2. high"])
+    Lows.append(tsd[Date]["3. low"])
+    Closes.append(tsd[Date]["4. close"])
+    Volumes.append(tsd[Date]["5. volume"])
 
 if "Error Message" in response.text:
-    print("OOPS couldn't find that symbol, please try again")
+    print("OOPS COULD NOT FIND THAT SYMBOL, PLEASE TRY AGAIN")
     exit()
 
-print(parsed_response)
+#print(parsed_response)
+#
+#for date, prices in tsd.items():
+#    print(date)
 
-tsd = parsed_response["Time Series (Daily)"]
-for date, prices in tsd.items():
-    print(date)
-    print(prices)
-    print("-------------------------")
+#    print(prices)
+#    print("-------------------------")
 
 print("-------------------------")
-print("SELECTED SYMBOL: XYZ")
+print("SELECTED SYMBOL: ", Symbol.upper())
 print("-------------------------")
 print("REQUESTING STOCK MARKET DATA...")
-print("REQUEST AT: 2018-02-20 02:00pm")
+print("REQUEST AT: ", date, time.strftime("%I:%M %p"))
 print("-------------------------")
-print("LATEST DAY: 2018-02-20")
-print("LATEST CLOSE: $100,000.00")
-print("RECENT HIGH: $101,000.00")
-print("RECENT LOW: $99,000.00")
+print("LATEST DAY: ", Dates[0])
+print("LATEST CLOSE: ", to_usd(eval(Closes[0])))
+print("RECENT HIGH: ", to_usd(eval(Highs[0])))
+print("RECENT LOW: ", to_usd(eval(Lows[0])))
 print("-------------------------")
 print("RECOMMENDATION: BUY!")
 print("RECOMMENDATION REASON: TODO")
