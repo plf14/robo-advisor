c931bbad3e5c461e63041bb296ad1dbbfa1c3c4e
plf14 <59637504+plf14@users.noreply.github.com>
2020-02-24 01:44:56 -0500

    Multiple Stocks

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -7,6 +7,7 @@ from dotenv import load_dotenv
 import datetime
 import plotly
 import plotly.graph_objs as go
+import csv
 
 load_dotenv()
 
@@ -25,83 +26,93 @@ time = datetime.datetime.now()
 
 print("-------------------------")
 print("WELCOME TO THE ROBO STOCK ADVISOR")
-print("ENTER THE SYMBOL OF YOUR STOCK TO RECIEVE MY RECOMENDATION")
-Symbol = input("SYMBOL: ")
+print("ENTER THE SYMBOL(S) OF YOUR STOCK(S) TO RECIEVE MY RECOMENDATION(S)")
+print("ENTER 'DONE' WHEN FINISHED")
+SymbolList = []
+condition = True
+while condition == True:
+    Symbol = input("SYMBOL: ")
+    if Symbol.lower() == 'done':
+        condition = False
+    else:
+        SymbolList.append(Symbol)
 
-if len(Symbol) > 5 or len(Symbol) < 1:
-    print("-------------------------")
-    print("EXPECTING A SYMBOL BETWEEN 1 AND 5 CHARACTERS, PLEASE TRY AGAIN")
-    print("-------------------------")
-    exit()
+for Symbol in SymbolList:
 
-for i in range(len(Symbol)):
-    if Symbol[i].isnumeric():
+    if len(Symbol) > 5 or len(Symbol) < 1:
         print("-------------------------")
-        print("EXPECTING A SYMBOL CONTATINING ONLY LETTERS, PLEASE TRY AGAIN")
+        print("EXPECTING A SYMBOL BETWEEN 1 AND 5 CHARACTERS, PLEASE TRY AGAIN")
         print("-------------------------")
         exit()
 
-print("-------------------------")
-print("REQUESTING SOME DATA FROM THE INTERNET...")
-print("-------------------------")
+    for i in range(len(Symbol)):
+        if Symbol[i].isnumeric():
+            print("-------------------------")
+            print("EXPECTING A SYMBOL CONTATINING ONLY LETTERS, PLEASE TRY AGAIN")
+            print("-------------------------")
+            exit()
 
-APIkey = os.getenv("ALPHAVANTAGE_API_KEY", default = "OOPS")
+    print("-------------------------")
+    print("REQUESTING SOME DATA FROM THE INTERNET...")
+    print("-------------------------")
 
-request_url = ("https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY&outputsize=full&symbol=" + Symbol + "&apikey=" + APIkey)
-print("URL:", request_url)
+    APIkey = os.getenv("ALPHAVANTAGE_API_KEY", default = "OOPS")
 
-response = requests.get(request_url)
-parsed_response = json.loads(response.text)
+    request_url = ("https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY&outputsize=full&symbol=" + Symbol + "&apikey=" + APIkey)
+    print("URL:", request_url)
 
-if "Error Message" in response.text:
-    print("OOPS COULD NOT FIND THAT SYMBOL, PLEASE TRY AGAIN")
-    exit()
+    response = requests.get(request_url)
+    parsed_response = json.loads(response.text)
 
-meta = parsed_response["Meta Data"]
-wsd = parsed_response["Weekly Time Series"]
+    if "Error Message" in response.text:
+        print("OOPS COULD NOT FIND THAT SYMBOL, PLEASE TRY AGAIN")
+        exit()
 
-Dates = list(wsd.keys())
-Open = []
-High = []
-Low = []
-Close = []
-Volume = []
+    meta = parsed_response["Meta Data"]
+    wsd = parsed_response["Weekly Time Series"]
 
-for Date in Dates:
-    Open.append(wsd[Date]["1. open"])
-    High.append(wsd[Date]["2. high"])
-    Low.append(wsd[Date]["3. low"])
-    Close.append(wsd[Date]["4. close"])
-    Volume.append(wsd[Date]["5. volume"])
+    Dates = list(wsd.keys())
+    Open = []
+    High = []
+    Low = []
+    Close = []
+    Volume = []
+    for Date in Dates:
+        Open.append(wsd[Date]["1. open"])
+        High.append(wsd[Date]["2. high"])
+        Low.append(wsd[Date]["3. low"])
+        Close.append(wsd[Date]["4. close"])
+        Volume.append(wsd[Date]["5. volume"])
 
-FiftyTwoHigh = max(High[0:53])
-FiftyTwoLow = min(Low[0:53])
-Lastest = Close[0]
+    FiftyTwoHigh = max(High[0:53])
+    FiftyTwoLow = min(Low[0:53])
+    Lastest = Close[0]
 
-#Write the logic for recomendation
+    #Write the logic for recomendation
 
-print("-------------------------")
-print("SELECTED SYMBOL: ", meta["2. Symbol"].upper())
-print("-------------------------")
-print("REQUESTING STOCK MARKET DATA...")
-print("REQUEST AT: ", date, time.strftime("%I:%M %p"))
-print("-------------------------")
-print("LATEST DAY: ", Dates[0])
-print("LATEST CLOSE: ", to_usd(eval(Close[0])))
-print("RECENT HIGH: ", to_usd(eval(High[0])))
-print("RECENT LOW: ", to_usd(eval(Low[0])))
-print("-------------------------")
-print("52-WEEK HIGH: ", to_usd(eval(FiftyTwoHigh)))
-print("52-WEEK LOW: ", to_usd(eval(FiftyTwoLow)))
-print("-------------------------")
-print("GENERATING LINE GRAPH...")
-plotly.offline.plot({
-    "data": [go.Scatter(x=Dates, y=Close)],
-    "layout": go.Layout(title="Line Data")
-}, auto_open=True)
-print("-------------------------")
-print("RECOMMENDATION: BUY!")
-print("RECOMMENDATION REASON: TODO")
+    print("-------------------------")
+    print("SELECTED SYMBOL: ", meta["2. Symbol"].upper())
+    print("-------------------------")
+    print("REQUESTING STOCK MARKET DATA...")
+    print("REQUEST AT: ", date, time.strftime("%I:%M %p"))
+    print("-------------------------")
+    print("LATEST DAY: ", Dates[0])
+    print("LATEST CLOSE: ", to_usd(eval(Close[0])))
+    print("RECENT HIGH: ", to_usd(eval(High[0])))
+    print("RECENT LOW: ", to_usd(eval(Low[0])))
+    print("-------------------------")
+    print("52-WEEK HIGH: ", to_usd(eval(FiftyTwoHigh)))
+    print("52-WEEK LOW: ", to_usd(eval(FiftyTwoLow)))
+    print("-------------------------")
+    print("GENERATING LINE GRAPH...")
+    plotly.offline.plot({
+        "data": [go.Scatter(x=Dates, y=Close)],
+        "layout": go.Layout(title= str(meta["2. Symbol"].upper()) + " WEEKLY CLOSE DATA")
+    }, auto_open=True)
+    print("-------------------------")
+    print("RECOMMENDATION: BUY!")
+    print("RECOMMENDATION REASON: TODO")
+
 print("-------------------------")
 print("HAPPY INVESTING!")
 print("-------------------------")
